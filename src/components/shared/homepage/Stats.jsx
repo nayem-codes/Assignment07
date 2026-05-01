22e338d3846cd3fdb8bc828ef9418a3a49df5a3c
nayem-codes <nayem83346@gmail.com>
2026-05-02 01:15:34 +0600

    modified stats section

--- src/components/shared/homepage/Stats.jsx
+++ src/components/shared/homepage/Stats.jsx
@@ -3,22 +3,22 @@ import React from 'react';
 const Stats = () => {
     return (
         <div className='bg-base-200'>
-            <div className='grid grid-cols-4 gap-6 bg-base-200 container mx-auto'>
-                <div className='bg-white shadow p-8 text-center'>
-                    <p>10</p>
+            <div className='grid sm:grid-cols-2 md:grid-cols-4 gap-6 bg-base-200 container mx-auto pt-10'>
+                <div className='bg-white shadow p-8 text-center rounded-md'>
+                    <p className='font-semibold text-3xl text-green-900'>8</p>
                     <p>Total friends</p>
                 </div>
-                <div className='bg-white shadow p-8 text-center'>
-                    <p>10</p>
-                    <p>Total friends</p>
+                <div className='bg-white shadow p-8 text-center rounded-md'>
+                    <p className='font-semibold text-3xl text-green-900'>6</p>
+                    <p>On Track</p>
                 </div>
-                <div className='bg-white shadow p-8 text-center'>
-                    <p>10</p>
-                    <p>Total friends</p>
+                <div className='bg-white shadow p-8 text-center rounded-md'>
+                    <p className='font-semibold text-3xl text-green-900'>2</p>
+                    <p>Need Attention</p>
                 </div>
-                <div className='bg-white shadow p-8 text-center'>
-                    <p>10</p>
-                    <p>Total friends</p>
+                <div className='bg-white shadow p-8 text-center rounded-md'>
+                    <p className='font-semibold text-3xl text-green-900'> 8</p>
+                    <p>Interactions This Month</p>
                 </div>
 
 
